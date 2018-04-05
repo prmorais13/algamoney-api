86bddadd2db1895e1678938aa609f435860ba3e2
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-04-05 17:43:35 -0300

    atualizacao

--- src/main/java/br/paulo/apicurso/repository/lancamento/LancamentosQuery.java
+++ src/main/java/br/paulo/apicurso/repository/lancamento/LancamentosQuery.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import br.paulo.apicurso.dto.LancamentoEstatisticaCategoria;
+import br.paulo.apicurso.dto.LancamentoEstatisticaDia;
 import br.paulo.apicurso.model.Lancamento;
 import br.paulo.apicurso.repository.filter.LancamentoFilter;
 import br.paulo.apicurso.repository.projection.ResumoLancamento;
@@ -14,6 +15,7 @@ import br.paulo.apicurso.repository.projection.ResumoLancamento;
 public interface LancamentosQuery {
 	
 	public List<LancamentoEstatisticaCategoria> porCategoria(LocalDate mesReferencia);
+	public List<LancamentoEstatisticaDia> porDia(LocalDate mesReferencia);
 	
 	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 	public Page<ResumoLancamento> resumo (LancamentoFilter filter, Pageable pageable);
