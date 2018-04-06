48331c724b4b9514ad7620079d405580f5752849
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-04-06 15:30:46 -0300

    Lancamento por pessoa

--- src/main/java/br/paulo/apicurso/model/TipoLancamento.java
+++ src/main/java/br/paulo/apicurso/model/TipoLancamento.java
@@ -2,7 +2,17 @@ package br.paulo.apicurso.model;
 
 public enum TipoLancamento {
 	
-	RECEITA,
-	DESPESA;
+	RECEITA ("Receita"),
+	DESPESA ("Despesa");
+	
+	private final String descricao;
+
+	private TipoLancamento(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
 
 }
